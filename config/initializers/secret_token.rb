d5a44b15eaadd59b639623306cd4fac691bd82f4
Michael Emhofer <emi@fsinf.at>
2013-01-17 20:38:18 +0100

    nu project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotolabChallenge::Application.config.secret_token = 'f6cfbbc046f8fd259cf6d1c5f69bde2e484a2e9e648bc81211e38e0add307165866464d596e63c887848617eb7e1efabb8b92e070f382ac555b7f4a05cca4454'
